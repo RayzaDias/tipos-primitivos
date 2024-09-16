8b94f5797a8338fa93f99f49281ece0a4bd2aef6
RayzaDias <rayzadiasalves@gmail.com>
2024-09-16 22:40:19 +0000

    atualizando código

--- aulas.js
+++ aulas.js
@@ -1,1 +1,16 @@
 
+/*
+ola = "Hello wordl"
+console.log(ola)
+*/
+
+//criei meu primeiro código
+const rotulo = "natalya"
+console.log(rotulo)
+rotulo = 12345
+console.log(rotulo)
+
+const num1 = 10
+const num2 = 20
+const calc = num1 - num2
+console .log("0 resultado é " + calc)
